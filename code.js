3d10162605870b97f86a4353feefbfa79b06e5cc
Valentin Squirelo <valentin+github@hackerloop.com>
2026-07-27 16:27:49 +0200

    Point configurator release link to MoveStick

--- code.js
+++ code.js
@@ -1608,7 +1608,7 @@ async function check_device_version() {
                 return true;
             }
             display_error_html('<p>Incompatible version (' + version + ').</p>' +
-                '<p>Please consider upgrading your HID Remapper firmware to the <a href="https://github.com/jfedor2/hid-remapper/releases/latest">latest version</a>.</p>' +
+                '<p>Please consider upgrading your HID Remapper firmware to the <a href="https://github.com/playAbilityTech/MoveStick/releases/latest">latest version</a>.</p>' +
                 '<p>Alternatively, click <a href="v' + version + '/">here</a> for an older version of the configuration tool that should be compatible with your device.</p>' +
                 '<p class="mb-0">(You can also use the older version of the configuration tool to put your HID Remapper in firmware flashing mode and upgrade the firmware.)</p>');
             return false;
